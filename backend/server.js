3daa1ce6aace28c1350ab9f3b9512b63097e7b4b
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-01 19:56:41 +0200

    testing route to get all users from database

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 8000;
 
+//importing user model
+const User = require('./models/user');
 
 // MongoDB connection
 // I haven't hidden password because it's a free account. Also, I do not want to spend too much time on backend :)
@@ -16,6 +18,16 @@ mongoose.connect('mongodb+srv://khatrishivani96:p9Nd3Wlxor1dzZcX@cluster0.h7ldig
 // Define routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/api/users', async(req, res) => {
+    try{
+        // Retrieve all users
+        const users = await User.find();
+        // Send the users as JSON response
+        res.json(users);
+    } catch {
+        res.status(500).json({"error" : "Error fetching users"});
+    }
 });
 
 // Start the server
