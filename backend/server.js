6e07e804837a1a5ead3b69c7b11865d5664bfba9
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-03 00:38:41 +0200

    route added to get notes specific to logged in user

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 //Body-parser is a middleware for Express.js that simplifies handling of HTTP request bodies
 const bodyParser = require("body-parser");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 //importing user model
 const User = require("./models/user");
 const Note = require("./models/note");
@@ -11,7 +11,7 @@ const Note = require("./models/note");
 const app = express();
 const port = process.env.PORT || 8000;
 // hardcoded secret key used for this project
-const SECRET_KEY = 'notetaking_app_key';
+const SECRET_KEY = "notetaking_app_key";
 
 // Middleware
 app.use(bodyParser.json());
@@ -32,28 +32,28 @@ mongoose
   .catch((err) => {
     console.log("Error connecting to MongoDB:", err);
   });
-  const extractToken = (authHeader) => {
-    if (authHeader && authHeader.startsWith("Bearer")) {
-        // Remove the "Bearer" prefix and return the token
-        return authHeader.substring(7);
-    } else {
-        // Handle the case where the header doesn't start with "Bearer"
-        return null;
-    }
-}
-  //Authentication middleware: to verify jwt token
-  const authenticateToken = (req, res, next) => {
-    const token = extractToken(req.headers.authorization);
-    if (!token) return res.status(401).json({ message: 'Unauthorized' });
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).json({
-            message: 'Forbidden'
-        })
-        req.user = user;
-        next();
-    })
+const extractToken = (authHeader) => {
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    // Remove the "Bearer" prefix and return the token
+    return authHeader.substring(7);
+  } else {
+    // Handle the case where the header doesn't start with "Bearer"
+    return null;
   }
-
+};
+//Authentication middleware: to verify jwt token
+const authenticateToken = (req, res, next) => {
+  const token = extractToken(req.headers.authorization);
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err)
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    req.user = user;
+    next();
+  });
+};
 
 // Define routes
 // Create a new user
@@ -69,22 +69,26 @@ app.post("/api/users", async (req, res) => {
 });
 
 // Login route to generate token
-app.post('/api/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        // username is unique
-        const user = await User.findOne({  username });
-        if( !user || !password ){
-            return res.status(401).json({message: "Invalid username or password"});
-        }
-        // Generating a JWT token for a user
-        const payload = { username: user.username, password: user.password, userId: user._id }
-        const token = jwt.sign(payload, SECRET_KEY);
-        res.json({ token });
-    } catch (error) {
-        res.status(500).json({message: "Internal server error", error});
+app.post("/api/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // username is unique
+    const user = await User.findOne({ username });
+    if (!user || !password) {
+      return res.status(401).json({ message: "Invalid username or password" });
     }
-})
+    // Generating a JWT token for a user
+    const payload = {
+      username: user.username,
+      password: user.password,
+      userId: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY);
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+});
 
 // Get all users
 app.get("/api/users", async (req, res) => {
@@ -103,7 +107,7 @@ app.post("/api/notes", authenticateToken, async (req, res) => {
   try {
     const { title, content } = req.body;
     // getting the user ID from 'req'
-    const userId = req.user.userId; 
+    const userId = req.user.userId;
     const newNote = new Note({ title, content, user: userId });
     await newNote.save();
     res.status(201).json(newNote);
@@ -112,13 +116,23 @@ app.post("/api/notes", authenticateToken, async (req, res) => {
     res.status(400).json({ message: "Error creating note", error });
   }
 });
-// Get all notes for specific user => add later...
+
+// Get notes for user that's logged in
+app.get("/api/notes", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const notes = await Note.find({ user: userId });
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(400).json({ message: "Error retrieving notes", error });
+  }
+});
 
 // Get all notes
 app.get("/api/notes", async (req, res) => {
   try {
     const notes = await Note.find({});
-    res.json(notes);
+    res.status(200).json(notes);
   } catch {
     res.status(500).json({ error: "Error fetching notes" });
   }
