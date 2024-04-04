aff83896dcc6d6b210ba080477cd0d9e1ada7e6d
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-05 01:09:26 +0200

    background color and header color chosen

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -3,20 +3,20 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <header>
+    <header class="navbar">
     <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="180" height="100" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <!-- <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
 </template>
 
 <style scoped>
-
+.navbar {
+    background-color: white;
+}
 </style>
