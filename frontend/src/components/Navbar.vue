c55b8be835e4591a95e4f11ec3461f2fdab1e363
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-04 23:55:38 +0200

    logo created

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,7 +4,7 @@ import { RouterLink } from 'vue-router'
 
 <template>
     <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="180" height="100" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
