ee4f0464508e84787411dc261346aafd4e0c89d0
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-05 18:33:58 +0200

    box-shadow added to navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,19 +4,21 @@ import { RouterLink } from 'vue-router'
 
 <template>
     <header class="navbar">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="180" height="100" />
+        <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="160" height="50" />
 
-    <!-- <div class="wrapper">
+        <!-- <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div> -->
-  </header>
+    </header>
 </template>
 
 <style scoped>
 .navbar {
-    background-color: white;
+    background-color: var(--white);
+    padding: 0.4rem;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 }
 </style>
