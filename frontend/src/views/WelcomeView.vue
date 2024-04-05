e781b4b1abdae93ab8ab1a77b841a30de92b0e9d
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-06 01:58:35 +0200

    notes image resized and reordered

--- frontend/src/views/WelcomeView.vue
+++ frontend/src/views/WelcomeView.vue
@@ -4,12 +4,12 @@
 
 <template>
     <main class="welcome-box">
+        <img alt="post-it picture" class="notes-image" src="@/assets/notes.png" />
         <div class="Noteworthy-box">
             <img alt="post-it picture" class="logo-icon" src="@/assets/logo.png" />
             <h1 class="welcome-title">Ready to embark on a note-taking journey? <br> Log in now to get started!</h1>
-            <button>Log in</button>
+            <button class="log-in-button">Log in</button>
         </div>
-        <img alt="post-it picture" class="notes-image" src="@/assets/notes.png" />
     </main>
 </template>
 
@@ -21,6 +21,7 @@
     flex-direction: row;
     align-items: center;
     justify-content: center;
+    gap: 1.5rem;
 }
 
 .Noteworthy-box {
@@ -44,6 +45,19 @@
 }
 
 .notes-image {
-    width: 30rem;
+    width: 20rem;
+}
+
+.log-in-button {
+    width: 8rem;
+    padding: 0.55rem 0;
+    border-radius: 8px;
+    font-weight: 700;
+    font-size: 1rem;
+}
+
+.log-in-button:hover {
+    cursor: pointer;
+    background-color: aqua;
 }
 </style>
