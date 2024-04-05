1e9951d3530b89ed2753700aac8960dc5b672a13
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-05 22:27:30 +0200

    notes images added to welcome page

--- frontend/src/views/WelcomeView.vue
+++ frontend/src/views/WelcomeView.vue
@@ -3,9 +3,27 @@
 </script>
 
 <template>
-  <main>
-   
-    Welcome
-
-  </main>
+    <main class="welcome-box">
+        <img alt="post-it picture" class="notes" src="@/assets/notes.png" />
+        <div>
+            <h1>Noteworthy</h1>
+           <p>Log in to create noteworthy Notes</p>
+           <button>Log in</button>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.welcome-box {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.notes {
+    width: 30rem;
+    /* width: 600px;
+    height: 600px; */
+}
+</style>
