346c8adfb503d1793034ef0968a26b82dfaec37f
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-08 20:33:40 +0200

    using v-show to toggle

--- frontend/src/components/NoteCreation.vue
+++ frontend/src/components/NoteCreation.vue
@@ -1,22 +1,37 @@
 <script setup>
+// import clickOutside from '../customDirective/clickOutside'; 
 import { ref } from "vue";
-const isCreatingNote = ref(true);
+const isCreatingNote = ref(false);
 const handleClick = () => {
-    isCreatingNote 
+    isCreatingNote.value = true;
+    console.log('ds', isCreatingNote.value)
 }
 const handleClickOutside = () => {
-    console.log('clicked outside');
+    if (isCreatingNote.value === true) {
+        console.log('outside');
+        isCreatingNote.value = false;
+    }
 }
 </script>
 <template>
     <div class="create-wrapper">
-        <div @click="handleClick" v-click-outside="handleClickOutside"e>click</div>
-        <!-- <div v-if="isCreatingNote" v-click-away="onClickAway">show this</div> -->
+        <div v-click-outside="handleClickOutside" class="create-wrapper">
+            <!-- using v-show because it doesn't work with v-if -->
+            <!-- v-show keeps the element in the DOM. So, this works efficiently -->
+            <div v-show="!isCreatingNote" @click="handleClick" class="divy">create</div>
+            <div v-show="isCreatingNote" class="divy">show</div>
+        </div>
     </div>
 </template>
 <style scoped>
 .create-wrapper {
     display: flex;
+    gap: 5rem;
     justify-content: center;
+}
+
+.divy {
+    padding: 1rem;
+    background-color: aqua;
 }
 </style>
