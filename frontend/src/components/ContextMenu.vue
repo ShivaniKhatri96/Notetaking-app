58ea4aa3b824344635faf8282431c9d8ff2209a0
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-08 15:39:50 +0200

    reusable contextMenu component created

--- frontend/src/components/ContextMenu.vue
+++ frontend/src/components/ContextMenu.vue
@@ -1,7 +1,25 @@
 <script setup>
+import { ref, defineProps, defineEmits } from 'vue';
+
+const { showMenu, menuX, menuY } = defineProps();
+//defineEmits provides a way to define the events that components can trigger
+const { emit } = defineEmits();
+
+const handleActionClick = (action) => {
+    emit('action-clicked', action);
+}
+ 
 </script>
 <template>
-
+    <div v-if="showMenu" class="context-menu" :style="{ top: `${menuY}px`, left: `${menuX}px` }">
+        <slot></slot>
+    </div>
 </template>
 <style scoped>
+.context-menu {
+    position: absolute;
+    background-color: blue;
+    padding: 0.5rem;
+    z-index: 100;
+}
 </style>
