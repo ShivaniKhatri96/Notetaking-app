5f05ea3621714f9deb33a5da8d36d20f5d186bd2
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-08 19:37:21 +0200

    comments added to clickOutside.js file for clarity

--- frontend/src/customDirective/clickOutside.js
+++ frontend/src/customDirective/clickOutside.js
@@ -1,26 +1,31 @@
 // clickOutside.js
 
 export default {
-    mounted(el, binding) {
-      // Click handler
-      const handleClick = (event) => {
-        if (!el.contains(event.target)) {
-          // Click occurred outside the element
-          binding.value(event);
-        }
-      };
-  
-      // Attach the event listener
-      document.addEventListener('click', handleClick);
-  
-      // Cleanup on unmount
-      el._clickOutsideCleanup = () => {
-        document.removeEventListener('click', handleClick);
-      };
-    },
-    unmounted(el) {
-      // Remove the event listener
-      el._clickOutsideCleanup();
-    },
-  };
-  
+  // mounted inserts component into the DOM
+  mounted(el, binding) {
+    // Click handler
+    const handleClick = (event) => {
+      //click occured outside the element
+      if (!el.contains(event.target)) {
+        // binding is an object that contains various properties related to the directive, including value
+        binding.value(event)
+      }
+    }
+
+    // Attaching the event listener
+    document.addEventListener('click', handleClick)
+
+    //Cleanup on mount
+    // the clickOutsideCleanup function is defined inside the mounted lifecycle hook
+    // to ensure that it is set up when the component is inserted into the DOM.
+    // This ensures that the event listener is properly cleaned up to prevent memory leaks or unexpected behavior
+    el._clickOutsideCleanup = () => {
+      document.removeEventListener('click', handleClick)
+    }
+  },
+  // unmounted removes component from the DOM
+  unmounted(el) {
+    // Removing the event listener
+    el._clickOutsideCleanup()
+  }
+}
