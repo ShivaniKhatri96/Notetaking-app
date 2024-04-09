1e2700ddee4c30f3ed5b981396942d34a0f20c38
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-09 03:31:35 +0200

    create note template is made responsive

--- frontend/src/components/NoteCreation.vue
+++ frontend/src/components/NoteCreation.vue
@@ -50,9 +50,16 @@ const cancelCreateNote = () => {
     gap: 1rem;
     justify-content: center;
     background-color: var(--white);
-    width: 40rem;
+    width: 100%;
     border-radius: 8px;
     box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+}
+
+/* Large devices (desktops, 992px and up) */
+@media (min-width: 992px) {
+    .create-wrapper {
+        width: 40rem;
+    }
 }
 
 .create-note-input {
