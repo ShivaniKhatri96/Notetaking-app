7223e30fb49a25113c41e37491c48a434708c108
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-09 02:15:34 +0200

    template for creating note updated

--- frontend/src/components/NoteCreation.vue
+++ frontend/src/components/NoteCreation.vue
@@ -17,7 +17,8 @@ const handleClickOutside = () => {
 }
 
 const handleCreateNote = () => {
-    console.log('create note');
+    const { title, content } = createNote.value;
+    console.log('create note', title + ':' + content);
 }
 
 </script>
@@ -25,16 +26,12 @@ const handleCreateNote = () => {
     <!-- using v-show because it doesn't work with v-if -->
     <!-- v-show keeps the element in the DOM. So, this works efficiently -->
     <div v-click-outside="handleClickOutside" class="create-wrapper" @click="handleClick">
-        <div v-show="!isCreatingNote" class="divy">
-            <input type="text" placeholder="Take a note..." v-model="createNote.content" />
-        </div>
-        <div v-show="isCreatingNote" class="divy">
-            <input type="text" placeholder="Title" v-model="createNote.title" />
-            <textarea rows="8" cols placeholder="Take a note..." v-model="createNote.content"></textarea>
-            <div>
-                <button>Cancel</button>
-                <button @click="handleCreateNote">Create</button>
-            </div>
+        <input v-show="isCreatingNote" type="text" placeholder="Title" v-model="createNote.title" />
+        <textarea class="create-note-input" :rows="isCreatingNote ? 8 : 1" cols placeholder="Take a note..."
+            v-model="createNote.content"></textarea>
+        <div v-show="isCreatingNote">
+            <button>Cancel</button>
+            <button @click="handleCreateNote">Create</button>
         </div>
     </div>
 </template>
@@ -50,8 +47,12 @@ const handleCreateNote = () => {
     box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
 }
 
-.divy {
-    /* padding: 1rem;
-    background-color: aqua; */
+.create-note-input {
+    width: 100%;
+    border: none;
+}
+
+.create-note-input:focus {
+    outline: none;
 }
 </style>
