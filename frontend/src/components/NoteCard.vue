4c09de1141161c03dbccc569761fc094c514bc27
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-10 13:38:42 +0200

    flexbox added to user info section(header) of note card

--- frontend/src/components/NoteCard.vue
+++ frontend/src/components/NoteCard.vue
@@ -25,9 +25,18 @@ defineProps({
 <template>
     <div class="card">
         <div :class="`card-header ${noteCreatorId === user?.userId ? 'current-user' : 'other-users'}`">
-            <div>{{ noteCreator }}</div>
-            <!-- <div>Public Note</div>
-            <div v-if="noteCreatorId === user?.userId">Edit</div> -->
+            <div class="flex-row">
+                <font-awesome-icon :icon="['fas', 'circle-user']" class="user-icon" />
+                <div class="note-creator">
+                    <div>{{ noteCreator }}</div>
+                    <div>Public Note</div>
+                </div>
+            </div>
+            <div v-if="noteCreatorId === user?.userId" class="flex-row">
+                <div>Edit</div>
+                <div>Delete</div>
+            </div>
+
         </div>
         <div class="content-box">
             <div>{{ title }}</div>
@@ -48,8 +57,16 @@ defineProps({
 }
 
 .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     padding: 1rem;
     box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+}
+
+.flex-row {
+    display: flex;
+    gap: 0.5rem;
 }
 
 .current-user {
