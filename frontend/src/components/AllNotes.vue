fe399b94164dd29e8fb8be423dd8a30c6671d369
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-10 04:29:40 +0200

    fetching all users moved to allNotes component instead of NoteCard

--- frontend/src/components/AllNotes.vue
+++ frontend/src/components/AllNotes.vue
@@ -1,14 +1,28 @@
 <script setup>
 import NoteCard from '../components/NoteCard.vue';
 import { useNotesStore } from '@/stores/notesStore';
+import { ref, onMounted } from "vue";
 
 const { notes, setNotes } = useNotesStore();
 console.log('notes from allNotes', notes);
 
+let noteCreators = ref([]);
+onMounted(async () => {
+    try {
+        const response = await fetch("http://localhost:8000/api/users");
+        if (response.ok) {
+            const data = await response.json();
+            noteCreators.value.push(data)
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
+})
 </script>
 <template>
     <div class="all-cards">
-        <NoteCard v-for="note in notes[0]" :key="note._id" :noteCreatorId="note.user"
+        <NoteCard v-for="note in notes[0]" :key="note._id"
+            :noteCreator="noteCreators[0]?.find(el => el._id === note.user).username" :noteCreatorId="note.user"
             :title="note?.title" :content="note.content" />
     </div>
 </template>
--- frontend/src/components/NoteCard.vue
+++ frontend/src/components/NoteCard.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import { useAuthStore } from '@/stores/auth';
 const { user } = useAuthStore();
 // import { useNotesStore } from '@/stores/notesStore';
 // const { notes, setNotes } = useNotesStore();
 defineProps({
     noteCreatorId: {
+        type: String,
+        required: true,
+    },
+    noteCreator: {
         type: String,
         required: true,
     },
@@ -18,23 +21,10 @@ defineProps({
         required: true,
     }
 })
-
-let noteCreators = ref([]);
-onMounted(async () => {
-    try {
-        const response = await fetch("http://localhost:8000/api/users");
-        if (response.ok) {
-            const data = await response.json();
-            noteCreators.value.push(data)
-        }
-    } catch (error) {
-        console.log('error', error);
-    }
-})
 </script>
 <template>
     <div>
-        <div>{{ noteCreators[0]?.find(el => el._id === noteCreatorId).username }}</div>
+        <div>{{ noteCreator }}</div>
         <div>Public Note</div>
         <div v-if="noteCreatorId === user.userId">Edit</div>
         <div>{{ title }}</div>
