431703fa56ed0a43136a4b9fd690cd603ba5bf29
Shivani Khatri <khatri.shivani96@gmail.com>
2024-04-13 22:19:01 +0200

    hamburger menu animation works

--- frontend/src/components/header/HamburgerMenu.vue
+++ frontend/src/components/header/HamburgerMenu.vue
@@ -1,55 +1,61 @@
 <script setup>
+import { ref } from "vue";
+
+const isMenuOpen = ref(false)
+const handleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+}
 </script>
 <template>
-    <div class="hamburger">
+    <div :class="`hamburger ${isMenuOpen ? 'hamburger-on' : ''}`" @click="handleMenu">
     </div>
 </template>
 <style scoped>
 /* Medium devices (tablets, less than 992px) */
 @media (max-width: 991.98px) {
+
+    /* when the menu isn't shown */
+    .hamburger,
+    .hamburger::before,
+    .hamburger::after {
+        width: 25px;
+        height: 2px;
+        background-color: var(--light-gray);
+        transition: transform 0.4s ease-in-out;
+    }
+
     .hamburger {
         position: relative;
-        display: inline-block;
-        width: 2.2rem;
-        height: 0.2rem;
-        /* margin-top: 1.1rem; */
-        background-color: #000;
-        transition: transform 0.4s ease-in-out;
     }
 
     .hamburger::before,
     .hamburger::after {
         content: "";
         position: absolute;
-        display: inline-block;
-        width: 2.2rem;
-        height: 0.2rem;
-        background-color: #000;
-        transition: transform 0.4s ease-in-out;
+        transform: rotate(0);
     }
 
     .hamburger::before {
-        top: -0.6rem;
-        transform: rotate(0);
+        top: -8px;
     }
 
     .hamburger::after {
-        top: 0.6rem;
-        transform: rotate(0);
+        top: 8px;
     }
 
-    .hamburger .hamburgerOn {
-        background-color: red;
+    /* when the menu is shown */
+    .hamburger-on {
+        background-color: var(--white);
     }
 
-    .hamburger .hamburgerOn::before {
+    .hamburger-on::before {
         top: 0rem;
-        transform: rotate(130deg);
+        transform: rotate(135deg);
     }
 
-    .hamburger .hamburgerOn::after {
+    .hamburger-on::after {
         top: 0rem;
-        transform: rotate(-130deg);
+        transform: rotate(-135deg);
     }
 }
 
