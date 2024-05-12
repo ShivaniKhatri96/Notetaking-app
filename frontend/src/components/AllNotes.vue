7770242b8f7d1992587dab21a130a83d015dc013
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-12 19:37:25 +0200

    only public notes are shown in home page. However, currently logged in user gets to see their private notes as well

--- frontend/src/components/AllNotes.vue
+++ frontend/src/components/AllNotes.vue
@@ -2,7 +2,9 @@
 import NoteCard from '../components/NoteCard.vue';
 import { useNotesStore } from '@/stores/notesStore';
 import { ref, onMounted } from "vue";
+import { useAuthStore } from '@/stores/auth';
 
+const authStore = useAuthStore();
 const { notes } = useNotesStore();
 let noteCreators = ref([]);
 onMounted(async () => {
@@ -16,10 +18,17 @@ onMounted(async () => {
         console.log('error', error);
     }
 })
+console.log(notes[0])
 </script>
 <template>
     <div class="all-cards">
-        <NoteCard v-for="note in notes[0]" :key="note._id" :noteId="note._id"
+        <!-- <NoteCard v-for="note in notes[0]?.filter(el => el.user === authStore.user?.userId)" :key="note._id" :noteId="note._id"
+            :noteCreator="noteCreators[0]?.find(el => el._id === note.user)?.username || ''" :noteCreatorId="note.user"
+            :title="note?.title" :content="note.content" :privacy="note.privacy" /> -->
+
+        <!-- only public notes are shown in home page. However, currently logged in user gets to see their private notes as well -->
+        <NoteCard v-for="note in notes[0]?.filter(el => el.privacy === false || el.user === authStore.user?.userId)"
+            :key="note._id" :noteId="note._id"
             :noteCreator="noteCreators[0]?.find(el => el._id === note.user)?.username || ''" :noteCreatorId="note.user"
             :title="note?.title" :content="note.content" :privacy="note.privacy" />
     </div>
