d0acb3855f718b858a6943a37ccc3b15013cb519
Shivani Khatri <khatri.shivani96@gmail.com>
2024-05-14 22:22:53 +0200

    testing by changing env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // replace({
-    //   'import.meta.env.BASE_URL': JSON.stringify(process.env.BASE_URL || 'https://notetaking-app-zeta.vercel.app/api')
-    // })
   ],
   resolve: {
     alias: {
